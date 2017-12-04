ec61c13839a3a006059c60444323c680abec7a1d
rafieruslan <32709698+rafieruslan@users.noreply.github.com>
2017-12-04 08:32:22 +0800

    Create Client.c

---
+++ Client.c
@@ -0,0 +1,35 @@
+#include<stdio.h> 
+#include<string.h>   
+#include<sys/socket.h>   
+#include<arpa/inet.h> 
+#include<netinet/in.h>
+ 
+int main(int argc,char **argv)
+{
+    int sockfd,n;
+    char sendline[100];
+    char recvline[100];
+    struct sockaddr_in servaddr;
+ 
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    bzero(&servaddr,sizeof servaddr);
+ 
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(22000);
+ 
+    inet_pton(AF_INET,"192.168.175.128",&(servaddr.sin_addr));
+ 
+    connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+ 
+    while(1)
+    {
+        bzero( sendline, 100);
+        bzero( recvline, 100);
+        fgets(sendline,100,stdin); /*stdin = 0 , for standard input */
+ 
+        write(sockfd,sendline,strlen(sendline)+1);
+        read(sockfd,recvline,100);
+        printf("%s",recvline);
+    }
+ 
+}
